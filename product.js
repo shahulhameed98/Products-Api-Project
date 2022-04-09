fe16e346c6ac23f41140379214673b2e39dd92d7
Shahul <shahulhameedk479@gmail.com>
2022-04-09 15:49:48 +0530

    Products-Api-Project Files Added

---
+++ product.js
@@ -0,0 +1,58 @@
+const productDOM = document.querySelector(".product");
+
+const url = "https://course-api.com/javascript-store-single-product";
+
+const fetchProduct = async () => {
+	productDOM.innerHTML = `<h4 class="product-loading">Loading...</h4>`;
+	try {
+		const params = new URLSearchParams(window.location.search);
+		const id = params.get("id");
+		const response = await fetch(`${url}?id=${id}`);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		productDOM.innerHTML = `<p class="error">There was a problem loading the product. Please try again later</h4>`;
+	}
+};
+
+const displayProduct = (product) => {
+	// company,colors,description,name:title,price,image(url:img)
+	const {
+		company,
+		colors,
+		description,
+		name: title,
+		price,
+		image,
+	} = product.fields;
+	const { url: img } = image[0];
+	document.title = title.toUpperCase();
+	//colors
+	const colorsList = colors
+		.map((color) => {
+			return `<span class="product-color" style="background: ${color}"></span>`;
+		})
+		.join("");
+	productDOM.innerHTML = `<div class="product-wrapper">
+				<img src="${img}" alt="${title}" class="img" />
+				<div class="product-info">
+					<h3>${title}</h3>
+					<h5>${company}</h5>
+					<span>$${price / 100}</span>
+					<div class="colors">
+						${colorsList}
+					</div>
+					<p>
+						${description}
+					</p>
+					<button class="btn">add to cart</button>
+				</div>
+			</div>`;
+};
+
+const start = async () => {
+	const data = await fetchProduct();
+	displayProduct(data);
+};
+
+start();
